3e8a3a3c3e71fee985e3309b3f1cd51464388c50
GeorgeFeng1227 <gf1021@nyu.edu>
2020-09-29 20:21:33 +0800

    Bug fix on right-click coloring

--- src/application_src/application_model/annotation/color/Rule.java
+++ src/application_src/application_model/annotation/color/Rule.java
@@ -108,6 +108,12 @@ public class Rule {
 
         this.rebuildSubsceneFlag = requireNonNull(rebuildSubsceneFlag);
 
+        //fetch the search term from the 'searched' parameter
+        if (searched.contains("'")) {
+            searched = searched.substring(searched.indexOf("'")+1, searched.lastIndexOf("'"));
+        }
+        final String searchTerm = searched;
+
         searchType = type;
         setOptions(options);
 
@@ -129,7 +135,7 @@ public class Rule {
                         case LINEAGE:
                             List<String> cells = new ArrayList<>();
                             CElegansSearchResults cElegansDataSearchResults = new CElegansSearchResults(
-                                    cElegansSearch.executeLineageSearch(searched,
+                                    cElegansSearch.executeLineageSearch(searchTerm,
                                             currAnc,
                                             currDes));
                             cells.addAll(cElegansDataSearchResults.getSearchResults());
@@ -139,7 +145,7 @@ public class Rule {
                              * in the event of a non-sulston embryo, you can search directly for
                              * the names of specific entities */
                             if (cells.isEmpty()) {
-                                cells.addAll(ModelSpecificSearchUtil.nonSulstonLineageSearch(searched,
+                                cells.addAll(ModelSpecificSearchUtil.nonSulstonLineageSearch(searchTerm,
                                         currAnc,
                                         currDes));
 
@@ -148,10 +154,10 @@ public class Rule {
                             setCells(cells);
                             break;
                         case FUNCTIONAL:
-                            setCells(cElegansSearch.executeFunctionalSearch(searched.substring(searched.indexOf("'")+1, searched.lastIndexOf("'")), currAnc, currDes, OrganismDataType.LINEAGE).getValue());
+                            setCells(cElegansSearch.executeFunctionalSearch(searchTerm, currAnc, currDes, OrganismDataType.LINEAGE).getValue());
                             break;
                         case DESCRIPTION:
-                            setCells(cElegansSearch.executeDescriptionSearch(searched.substring(searched.indexOf("'")+1, searched.lastIndexOf("'")), currAnc, currDes, OrganismDataType.LINEAGE).getValue());
+                            setCells(cElegansSearch.executeDescriptionSearch(searchTerm, currAnc, currDes, OrganismDataType.LINEAGE).getValue());
                             break;
                         case GENE:
                             System.out.println("Updating gene rule from RuleEditorController is currently not supported.\nPlease delete the rule and run an updated search from the Find Cells tab. (07/2018)");
@@ -173,7 +179,7 @@ public class Rule {
                             if (text.contains("neuro")) {
                                 neuro = true;
                             }
-                            setCells(cElegansSearch.executeConnectomeSearch(searched.substring(searched.indexOf("'")+1, searched.lastIndexOf("'")), currAnc, currDes, pre, post, elec, neuro, OrganismDataType.LINEAGE).getValue());
+                            setCells(cElegansSearch.executeConnectomeSearch(searchTerm, currAnc, currDes, pre, post, elec, neuro, OrganismDataType.LINEAGE).getValue());
                             break;
                     }
                 }
